63ac97b0402e4b67cac79f23ed586fbfb771e33e
Nqla <ethancarreon621@gmail.com>
2024-09-20 22:44:29 -0400

    Task 1B Completed

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -9,12 +9,52 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public float maxSpeed = 5f;
+    public float currentSpeed = 0f;
+    public float accelerationTime = 3f;
+    float acceleration;
+
+    void Start()
+    {
+        acceleration = maxSpeed / accelerationTime;
+    }
+
     void Update()
     {
-        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Vector3 newPos = transform.position + velocity * 2 * Time.deltaTime;
 
-        transform.position = newPos;
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // Task 1A
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        // Task 1B
+
+        if (movement.magnitude > 0)
+        {
+            currentSpeed = currentSpeed + acceleration * Time.deltaTime;
+
+            if (currentSpeed > maxSpeed)
+            {
+                currentSpeed = maxSpeed;
+            }
+        }
+        else
+        {
+            currentSpeed -= acceleration * Time.deltaTime;
+
+            if (currentSpeed < 0)
+            {
+                currentSpeed = 0;
+            }
+        }
+
+        Vector3 velocity = movement * currentSpeed;
+        transform.position += velocity * Time.deltaTime;
+
     }
 
 }
