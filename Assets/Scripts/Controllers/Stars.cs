91041511855cf1045e2cd57d885b617ee3498f40
Nqla <ethancarreon621@gmail.com>
2024-09-21 03:41:28 -0400

    Task 4 Completed

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,9 +6,39 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
+    float currentTime = 0f;
 
-    // Update is called once per frame
+    int currentStartStar = 0;
+    int currentEndStar = 1;
+
     void Update()
     {
+        DrawConstellation();
+    }
+
+    void DrawConstellation()
+    {
+        currentTime += Time.deltaTime;
+        float t = currentTime / drawingTime;
+
+        Vector3 endPoint = Vector3.Lerp(starTransforms[currentStartStar].position, starTransforms[currentEndStar].position, t);
+
+        Debug.DrawLine(starTransforms[currentStartStar].position, endPoint, Color.white);
+
+        if (t >= 1f)
+        {
+            currentStartStar = currentEndStar;
+            currentEndStar++;
+
+            currentTime = 0f;
+        }
+
+        if (currentEndStar >= starTransforms.Count)
+        {
+            currentEndStar = 1;
+            currentStartStar = 0;
+        }
+
+        
     }
 }
